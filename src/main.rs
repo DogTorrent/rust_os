bf380f902b886acca037bb9dea3001ddc34e9c18
DotTorrent <pointtorrent@outlook.com>
2020-08-28 19:23:38 +0800

    Create a minimal rust kernel
    Create a minimal 64-bit Rust kernel for the x86 architecture.
    (follow the guide from https://os.phil-opp.com/minimal-rust-kernel/)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 #![no_std]
 #![no_main]
 
+extern crate rlibc;
 
 use core::panic::PanicInfo;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     // This function is called on panic
+
     loop {}
 }
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // This function is the entry point
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
