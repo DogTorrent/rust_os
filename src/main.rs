af1bbf0f01b6f56164e8756a9de776622e6d73a8
DotTorrent <pointtorrent@outlook.com>
2020-11-15 01:53:22 +0800

    Create an interface that wraps VGA text mode.
    1. Create an interface that makes the usage of the VGA text mode safe and simple.
    2. Implement support for Rust's formatting macros(`print!` and `println!`).
    3. Setting cargo's build-std-features flag to ["compiler-builtins-mem"] to enable some memory-related functions in the 'compiler_builtins' crate that are not enabled by default.
    (see https://os.phil-opp.com/minimal-rust-kernel/#memory-related-intrinsics )

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
 #![no_std]
 #![no_main]
 
-extern crate rlibc;
+mod vga_buffer; // Create a Rust module to handle printing
 
 use core::panic::PanicInfo;
 
 /// This function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 /// This function is the entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    println!("Hello World{}", "!");
+    loop{}
 }
-
-mod vga_buffer; // Create a Rust module to handle printing
