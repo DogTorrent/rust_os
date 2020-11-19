94d034078e395a9e6f0c06405c09cbea7f2c9f69
DotTorrent <pointtorrent@outlook.com>
2020-11-19 16:07:17 +0800

    Switch from x86_64 to arm64
    remove a previous test case using custom test framework and dependencies related to x86_64

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@
 
 use core::panic::PanicInfo;
 
-pub mod serial;
 pub mod vga_buffer; // Create a Rust module to handle printing
 
 /// This function is called on panic(test mode)
@@ -33,40 +32,22 @@ where
     T: Fn(),
 {
     fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
+        print!("{}...\t", core::any::type_name::<T>());
         self();
-        serial_println!("[ok]");
+        println!("[ok]");
     }
 }
 
 /// This function is the entry function of test framework
 pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
+    println!("Running {} tests", tests.len());
     for test in tests {
         test.run();
     }
-    exit_qemu(QemuExitCode::Success);
 }
 
 pub fn test_panic_handler(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
+    println!("[failed]\n");
+    println!("Error: {}\n", info);
     loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
